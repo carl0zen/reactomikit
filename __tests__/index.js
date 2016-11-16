11c9a0548a8fcbd6ea750472fbc4aa5b39bb86ca
Sergio del Rio <sdelrio0@gmail.com>
2016-11-16 08:49:04 -0600

    Make some jsdoc tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,60 +1,144 @@
-import React from 'react';
+import { expect } from 'chai'
+import { parse, parseFile, listDirRecursive, buildUiKitAST } from '../lib/jsDocs'
+import path from 'path'
 
-import {shallow, mount, render} from 'enzyme';
-import {expect} from 'chai';
-import sinon from 'sinon';
-
-import App from '../index';
-
-// Demo tests
-
-// Shallow Rendering
-// https://github.com/airbnb/enzyme/blob/master/docs/api/shallow.md
-describe('Shallow Rendering', () => {
-
-    it('to have three `.icon-test`s', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find('.icon-test')).to.have.length(3);
-    });
-
-    it('simulates click events', () => {
-        const buttonClick = sinon.spy();
-        const wrapper = shallow(
-          <App handleClick={buttonClick} />
-        );
-        wrapper.find('button').simulate('click');
-        expect(buttonClick.calledOnce).to.equal(true);
-    });
-
-});
-
-// Full DOM Rendering
-// https://github.com/airbnb/enzyme/blob/master/docs/api/mount.md
-describe('Full DOM Rendering', () => {
-
-    it('allows us to set props', () => {
-        const wrapper = mount(<App bar='baz' />);
-        expect(wrapper.props().bar).to.equal('baz');
-        wrapper.setProps({ bar: 'foo' });
-        expect(wrapper.props().bar).to.equal('foo');
-    });
-
-    it('calls componentDidMount', () => {
-        sinon.spy(App.prototype, 'componentDidMount');
-        const wrapper = mount(<App />);
-        expect(App.prototype.componentDidMount.calledOnce).to.be.true;
-        App.prototype.componentDidMount.restore();
-    });
-
-});
-
-// Static Rendered Markup
-// https://github.com/airbnb/enzyme/blob/master/docs/api/render.md
-describe('Static Rendered Markup', () => {
-
-    it('renders three `.icon-test`s', () => {
-        const wrapper = render(<App />);
-        expect(wrapper.find('.icon-test').length).to.equal(3);
-    });
-
-});
+describe('JsDoc Parser', function() {
+  it('should parse jsDocs from a file', () => {
+    const fileName = path.join(process.cwd(), 'examples', 'ui', 'core.js')
+    const expected = [
+      {
+        "variables": [
+          {
+            "name": "uikit",
+            "value": "Button"
+          },
+          {
+            "name": "import",
+            "value": "Button"
+          },
+          {
+            "name": "description",
+            "value": "This is a normal button, part of the core components."
+          },
+          {
+            "name": "category",
+            "value": "core"
+          }
+        ],
+        "code": "export const Button = (props) => (\n  <button value={props.value} />\n)"
+      },
+      {
+        "variables": [
+          {
+            "name": "uikit",
+            "value": "Input"
+          },
+          {
+            "name": "import",
+            "value": "Input"
+          },
+          {
+            "name": "description",
+            "value": "This is a text input, part of the core components."
+          },
+          {
+            "name": "category",
+            "value": "core"
+          }
+        ],
+        "code": "export const Input = (props) => (\n  <input type=\"text\" placeholder=\"My input\"/>\n)"
+      },
+      {
+        "variables": [
+          {
+            "name": "description",
+            "value": "This should not be included in the uikit"
+          }
+        ],
+        "code": "export const helper = () => {\n  \n}"
+      }
+    ]
+  
+  
+    return new Promise((resolve, reject) => {
+      parseFile(fileName).then((ast) => {
+        resolve(Promise.all([
+          expect(ast).to.eql(expected)
+        ]))
+      }).catch((err) => reject(err))
+    })
+  })
+  
+  it('should get all files on a directory', () => {
+    return new Promise((resolve, reject) => {
+      listDirRecursive(path.join(process.cwd(), 'examples', 'ui'), ['js', 'jsx']).then((list) => {
+        resolve(Promise.all([
+          expect(list).to.eql([
+            process.cwd() + '/examples/ui/core.js',
+            process.cwd() + '/examples/ui/form.js',
+            process.cwd() + '/examples/ui/nested/composer.js',
+          ])
+        ]))
+      }).catch((err) => reject(err))
+    })
+  })
+  
+  it('should build the ui kit ast given a diectory', () => {
+    const expected = {
+      "categories": [
+        {
+          "name": "core",
+          "components": [
+            {
+              "name": "Button",
+              "importName": "Button",
+              "importPath": "/Users/sdelrio/src/_tinker/reactomikit/examples/ui/core.js",
+              "description": "This is a normal button, part of the core components.",
+              "code": "export const Button = (props) => (\n  <button value={props.value} />\n)"
+            },
+            {
+              "name": "Input",
+              "importName": "Input",
+              "importPath": "/Users/sdelrio/src/_tinker/reactomikit/examples/ui/core.js",
+              "description": "This is a text input, part of the core components.",
+              "code": "export const Input = (props) => (\n  <input type=\"text\" placeholder=\"My input\"/>\n)"
+            }
+          ]
+        },
+        {
+          "name": "form",
+          "components": [
+            {
+              "name": "Form",
+              "importName": "default",
+              "importPath": "/Users/sdelrio/src/_tinker/reactomikit/examples/ui/form.js",
+              "description": "This is a regular form.",
+              "code": "export default (props) => (\n  <form>\n    {props.children}\n  </form>\n)"
+            }
+          ]
+        },
+        {
+          "name": "nested",
+          "components": [
+            {
+              "name": "Composer",
+              "importName": "default",
+              "importPath": "/Users/sdelrio/src/_tinker/reactomikit/examples/ui/nested/composer.js",
+              "description": "This is a more complex component.",
+              "code": "export default (props) => (\n  <Form>\n    <Input />\n    <Input />\n    <Button value=\"Submit\" />\n  </Form>\n)"
+            }
+          ]
+        }
+      ]
+    }
+  
+  
+    return new Promise((resolve, reject) => {
+      buildUiKitAST(path.join(process.cwd(), 'examples', 'ui'), ['js', 'jsx']).then((ast) => {
+        resolve(Promise.all([
+          expect(ast).to.eql(expected)
+        ]))
+      }).catch((err) => reject(err))
+    })
+  })
+})
